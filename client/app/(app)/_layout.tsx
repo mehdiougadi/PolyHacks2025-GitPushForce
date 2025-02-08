e0f9219c025c59bdb73d290bf10670b4a8267672
mehdiougadi <Mehdiougadiofficial@gmail.com>
2025-02-08 14:23:35 -0500

    Mehdi: update

--- client/app/(app)/_layout.tsx
+++ client/app/(app)/_layout.tsx
@@ -5,7 +5,9 @@ import { View } from "react-native";
 export default function AppLayout(){
 
     return (
-            <Tabs>
+            <Tabs screenOptions={{
+                headerShown: false,
+            }}>
                 <Tabs.Screen name="home/index" />
             </Tabs>
     );
--- client/app/(auth)/_layout.tsx
+++ client/app/(auth)/_layout.tsx
@@ -5,7 +5,9 @@ import { View } from "react-native";
 export default function AuthLayout(){
 
     return (
-            <Tabs>
+            <Tabs screenOptions={{
+                headerShown: false,
+            }}>
                 <Tabs.Screen name="entry/index" />
                 <Tabs.Screen name="signin/index" />
                 <Tabs.Screen name="signup/index" />

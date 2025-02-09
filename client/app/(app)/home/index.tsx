5ea9b7cbce5098fff230deeedc23b658cf60327b
mehdiougadi <Mehdiougadiofficial@gmail.com>
2025-02-09 05:11:07 -0500

    Mehdi: added title for home page

--- client/app/(app)/home/index.tsx
+++ client/app/(app)/home/index.tsx
@@ -1,11 +1,24 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Platform } from "react-native";
 import DefaultScreen from "@client/components/screens/default-screen";
 import features from "@client/constants/features";
 import FeatureItem from "@client/components/items/feature-item";
+import SansitaText from "@client/components/texts/sansita-text";
 
 export default function Home() {
+    const titleSize = Platform.select({
+        web: "48",
+        default: "32"
+    });
+
     return (
         <DefaultScreen>
+            <View style={{flexDirection: "row", justifyContent: 'center', padding: 16}}>
+                <SansitaText 
+                    text="FarmerFlow" 
+                    fontSize={titleSize} 
+                    lineHeight="auto"
+                />
+            </View>
             <View style={styles.container}>
                 {features.map((feature) => (
                     <FeatureItem key={feature.route} feature={feature} />
--- client/components/items/feature-item.tsx
+++ client/components/items/feature-item.tsx
@@ -1,19 +1,34 @@
-import { StyleSheet, View, Text, Image, Touchable, TouchableOpacity } from "react-native";
+import { StyleSheet, View, Text, Image, TouchableOpacity, Platform } from "react-native";
 import Feature from "@common/interfaces/feature";
 import { useRouter } from "expo-router";
 
 export default function FeatureItem({ feature }: { feature: Feature }) {
     const router = useRouter();
-  return (
-    <TouchableOpacity style={styles.container} onPress={() => {router.navigate(feature.route as any)}}>
-      <Image source={feature.icon} style={styles.icon} />
-      <Text style={styles.name}>{feature.name}</Text>
-    </TouchableOpacity>
-  );
+    
+    return (
+        <TouchableOpacity 
+            style={[styles.container, Platform.select({
+                web: styles.containerWeb
+            })]} 
+            onPress={() => {router.navigate(feature.route as any)}}
+        >
+            <Image 
+                source={feature.icon} 
+                style={[styles.icon, Platform.select({
+                    web: styles.iconWeb
+                })]} 
+            />
+            <Text style={[styles.name, Platform.select({
+                web: styles.nameWeb
+            })]}>
+                {feature.name}
+            </Text>
+        </TouchableOpacity>
+    );
 }
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
@@ -26,12 +41,25 @@ const styles = StyleSheet.create({
         shadowRadius: 4,
         elevation: 5,
     },
-    icon:{
+    containerWeb: {
+        padding: 32,
+        cursor: 'pointer',
+    },
+    icon: {
         height: 125,
         width: 125,
         borderRadius: 12,
     },
-    name:{
+    iconWeb: {
+        height: 200,
+        width: 200,
+    },
+    name: {
         fontSize: 16,
+        marginTop: 12,
+    },
+    nameWeb: {
+        fontSize: 24,
+        marginTop: 16,
     }
 });

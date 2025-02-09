38fa5d3ba5234e998b9bf67cc3948142eca3f356
Pepen0 <115287672+Pepen0@users.noreply.github.com>
2025-02-08 20:44:43 -0500

    changed format to ts and parse it now

--- client/utils/data-parser.ts
+++ client/utils/data-parser.ts
@@ -1,20 +1,34 @@
-import { Farm_Item_Prices, Farm_Item_Quantities } from '../data';
+import { 
+    Farm_Item_Prices, 
+    priceColumns,
+  } from '../data/Farm_Item_Prices';
 
+import { 
+    Farm_Item_Quantities,
+    quantityColumns 
+  } from '../data/Farm_Item_Quantities';
+  
 interface DataPoint {
-  date: Date;
-  value: number;
-}
-
+    date: Date;
+    value: number;
+  }
+  
 export const parsePriceData = (itemName: string): DataPoint[] => {
-  return Farm_Item_Prices.map(row => ({
-    date: new Date(row[0]),
-    value: row[itemName as keyof typeof Farm_Item_Prices[0]] as number
-  }));
-};
-
+    const columnIndex = priceColumns.indexOf(itemName);
+    if (columnIndex === -1) return [];
+    
+    return Farm_Item_Prices.map(row => ({
+      date: new Date(row[0]),
+      value: row[columnIndex + 1] as number
+    }));
+  };
+  
 export const parseQuantityData = (itemName: string): DataPoint[] => {
-  return Farm_Item_Quantities.map(row => ({
-    date: new Date(row[0]),
-    value: row[itemName as keyof typeof Farm_Item_Quantities[0]] as number
-  }));
-};
+    const columnIndex = quantityColumns.indexOf(itemName);
+    if (columnIndex === -1) return [];
+    
+    return Farm_Item_Quantities.map(row => ({
+      date: new Date(row[0]),
+      value: row[columnIndex + 1] as number
+    }));
+  };
